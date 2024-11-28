3f139a8aea00dbf6b3e0669596461d7de2cfdba7
zeynepaltintas27 <zynpaltnts27@gmail.com>
2024-11-28 03:38:05 +0300

    Initial commit

---
+++ phonon/dispersion.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load band data from Phonopy
+data = np.loadtxt("band.dat")
+
+# Extract x-coordinates and phonon frequencies
+x = data[:, 0]  # Reciprocal space
+frequencies = data[:, 1:]  # Phonon frequencies
+
+# Plot the phonon dispersion
+for i in range(frequencies.shape[1]):
+    plt.plot(x, frequencies[:, i], label=f"Mode {i+1}")
+plt.xlabel("Wave Vector")
+plt.ylabel("Frequency (THz)")
+plt.title("Phonon Dispersion Relation")
+plt.grid()
+plt.show()
+plt.savefig("phonon_dispersion.png")
